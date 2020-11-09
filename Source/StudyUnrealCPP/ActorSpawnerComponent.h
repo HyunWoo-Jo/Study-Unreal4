084f34f5776f30ef0cac30cf7d6cd5a47e37b320
HyunWooJo <CoreHyunWoo@gmail.com>
2020-11-09 18:30:56 +0900

    Scene Component
    SceneComponent 사용해보기

---
+++ Source/StudyUnrealCPP/ActorSpawnerComponent.h
@@ -0,0 +1,34 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/SceneComponent.h"
+#include "ActorSpawnerComponent.generated.h"
+
+
+UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
+class STUDYUNREALCPP_API UActorSpawnerComponent : public USceneComponent
+{
+	GENERATED_BODY()
+
+public:	
+	// Sets default values for this component's properties
+	UActorSpawnerComponent();
+
+protected:
+	// Called when the game starts
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
+
+	UPROPERTY(EditAnywhere)
+		TSubclassOf<AActor> ActorToSpawn;
+
+	UFUNCTION(BlueprintCallable, Category = Cookbook)
+		void Spawn();
+
+		
+};
